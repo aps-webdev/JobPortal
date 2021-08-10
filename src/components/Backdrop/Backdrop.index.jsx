bd3a82e513e0b5b57f1250b0809e1ecb92acbf03
Aman <amanprataps31@gmail.com>
2021-08-10 19:33:34 +0530

    Updates

--- src/components/Backdrop/Backdrop.index.jsx
+++
@@ -1,5 +0,0 @@
-import './Backdrop.styles.scss';
-
-export default function Backdrop() {
-  return <div className='backdrop' />;
-}
--- src/helpers/getQueryParam.js
+++
@@ -1,7 +0,0 @@
-export const getQueryParam = (params) => {
-  return Object.entries(params)
-    .map((param) => {
-      return `${param[0]}=${param[1]}`;
-    })
-    .join('&');
-};
